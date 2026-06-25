5a91a7d7e535329b49e6e65f032a989751f6e29f
Rayan Dhifallah <133664078+rayan951@users.noreply.github.com>
2026-06-25 22:18:55 +0200

    Delete Obox-frontend (1)/Obox-frontend directory

--- Obox-frontend (1)/Obox-frontend/app.py
+++
@@ -1,8 +0,0 @@
-from server import create_app
-
-# Création de l'application via la factory
-app = create_app()
-
-if __name__ == '__main__':
-    # Lancement du serveur
-    app.run(debug=True, port=5000)
